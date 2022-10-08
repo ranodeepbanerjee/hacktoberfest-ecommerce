f7121168be578d9e6351ef51c64aa81630dbd41e
Ranodeep Banerjee <63450189+ranodeepbanerjee@users.noreply.github.com>
2022-10-08 18:40:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import Product from "./pages/Product";
+
 import Home from "./pages/Home"
-import ProductList from "./pages/ProductList"
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
 
 const App = () => {
   return <Home/>;
